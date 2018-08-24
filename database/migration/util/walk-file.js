97aecbe68aa8888bf84a8d8f6c4fad5ac29ab75d
fankeke007 <610047449@qq.com>
2018-08-24 12:59:30 +0800

    migration

---
+++ database/migration/util/walk-file.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+//遍历目录下文件目录
+const walkFile = function(pathResolve,mime){
+    let files = fs.readdirSync(pathResolve);
+    let fileList = {};
+
+    for(let [i,item] of files.entries()){
+        let itemArr = item.split('\.');
+
+        let itemMime = (itemArr.length>1)?itemArr[itemArr.length-1]:'undefiend';
+        let keyName = item + '';
+        if(mime === itemMime){
+            fileList[item] = pathResolve + item;
+        }
+    }
+    return fileList;
+}
+
+module.exports = walkFile;
